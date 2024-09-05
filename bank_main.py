f3d42a7e07cd9ddc13a3f1106843c2edb9c96450
Thicasella <thiago.mcasella@gmail.com>
2024-09-04 23:07:55 -0300

    primeiro commit

---
+++ bank_main.py
@@ -0,0 +1,10 @@
+print('olá, bem vindo a banco itacu')
+print('digite o numero da sua conta e sua sennha')
+
+login = [thiago, jonathan]
+senha = [1234,7994]
+
+class usuario:
+    nome = ''
+    cpf = ''
+    id = ''
