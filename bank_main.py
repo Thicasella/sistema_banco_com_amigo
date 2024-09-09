dd54a34dd13b7ccac972bf78cadf94c735a6fff6
Jonathan <jhonata.moia@gmail.com>
2024-09-09 17:21:22 -0300

    Setimo Commit - Atualização e melhoria

--- bank_main.py
+++ bank_main.py
@@ -1,6 +1,10 @@
+import os 
+import getpass
+
 # Lista de usuários e senhas
 usuarios = ["thiago", "john"]
 senhas = [2112, 1707]
+valor = [2500.00, 3500.00]
 
 # Mensagem de boas-vindas
 print('Olá, bem-vindo ao Banco Itacu')
@@ -13,8 +17,12 @@ acesso_senha = 0
 
 while True:
     acesso_nome = input("Digite seu usuário: ").strip().lower()
-    acesso_senha = int(input("Digite sua senha: ").strip())
+    acesso_senha = getpass.getpass("Digite sua senha: ").strip()
+    #acesso_senha = int(input("Digite sua senha: ").strip())
     
+    # Convertendo a senha para inteiro para comparação
+    acesso_senha = int(acesso_senha)
+
     # Verificando se o usuário existe na lista
     if acesso_nome in usuarios:
         # Encontrando o índice do usuário na lista
@@ -30,5 +38,43 @@ while True:
         print("Usuário não encontrado. Tente novamente.")
 
 # Continuar o fluxo após o login bem-sucedido
-print("Bem vindo {}, o que gostaria de fazer? ".format(acesso_nome))
-print("Sacar, Extrato ou Depositar")
+print("Bem vindo(a) {}, o que gostaria de fazer? ".format(acesso_nome))
+
+entrada = int(input('1 para sacar\n2 para saldo\n3 para depositar \n0 para sair\nDigite seu escolha: '))
+
+def saldo():
+    os.system("cls")
+    print('Você selecionou saldo ')
+    print('Olá, senhor(a) {}, seu saldo é R${}'.format(acesso_nome, valor[indice]))
+
+def saque():
+    os.system("cls")
+    print('Você selecionou sacar ')
+    saque= float(input(print('Quanto você deseja sacar? : ')))
+    novo_saldo =  valor[indice] - saque
+    print('Você sacou {}, seu saldo atual é {}'.format(saque, novo_saldo))
+    #valor[indice] += saque
+
+def deposito():
+    os.system("cls")
+    print('Você escolheu depositar')
+    depositar= float(input(print('quanto você deseja depositar? : ')))
+    novo_deposito =  valor[indice] + depositar
+    print('Você depositou {}, seu saldo atual é {}'.format(depositar, novo_deposito))
+    #valor[indice] += depositar
+
+if entrada == 1:
+    saque()
+
+elif entrada == 2:
+    saldo()
+
+elif entrada == 3:
+    deposito()
+
+elif entrada == 0:
+    print('Você selecionou sair')
+    print('Ate mais Sr.(a) {}'.format(acesso_nome))
+    exit()
+else:
+    print('Não existe essa opção')
